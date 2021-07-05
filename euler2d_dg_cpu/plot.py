3107f073fe77143bc45cea71b0549f62fe8efb51
macfadyen <macfadyen@nyu.edu>
2021-07-04 21:13:05 -0400

    2d DG

---
+++ euler2d_dg_cpu/plot.py
@@ -0,0 +1,12 @@
+import sys
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
+x, y, rho, vx, vy, pre = np.loadtxt(sys.argv[1]).T
+ni, nj = [math.isqrt(len(x))] * 2
+x, y, rho, vx, vy, pre = [a.reshape([ni, nj]) for a in [x, y, rho, vx, vy, pre]]
+
+plt.plot(rho[256,:])
+#plt.imshow(rho)
+plt.show()
